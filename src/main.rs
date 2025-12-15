6cb20428b43f02cf3ee3d3fce296bbca49b072dc
J <133714506+J-Feltens@users.noreply.github.com>
2025-12-15 22:59:16 +0100

    gna multithread again

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufWriter, Error, ErrorKind, Write};
 use tqdm::pbar;
+
+const DEFAULT_RANGE: (u64, u64) = (1, 10_000);
 
 fn is_euler_triangle(a: u64, b: u64) -> bool {
     // calc diagonals squared
@@ -13,19 +18,29 @@ fn is_duplicate(a: u64, b: u64, triangles: &Vec<(u64, u64)>) -> bool {
             return true;
         }
     }
-    return false;
+    false
 }
 
-fn find_euler_triangles(a_range: (u64, u64), b_range: (u64, u64)) -> Vec<(u64, u64)> {
-    assert!(a_range.0 <= a_range.1);
-    assert!(b_range.0 <= b_range.1);
+fn store_triangles(triangles: &Vec<(u64, u64)>, path: &str) {
+    let mut f = File::create(path).expect("Failed to write to file");
+    let mut writer = BufWriter::new(f);
+
+    writeln!(writer, "{}", triangles.len()).unwrap();
+
+    for triangle in triangles.iter() {
+        writeln!(writer, "{},{}", triangle.0, triangle.1).unwrap();
+    }
+}
+
+fn find_euler_triangles(range: (u64, u64)) -> Vec<(u64, u64)> {
+    assert!(range.0 <= range.1);
 
     let mut triangles: Vec<(u64, u64)> = Vec::new();
 
-    let mut pbar = pbar(Some((a_range.1 - a_range.0) as usize));
-    for a in a_range.0..a_range.1 {
+    let mut pbar = pbar(Some((range.1 - range.0) as usize));
+    for a in range.0..range.1 {
         pbar.update(1).unwrap();
-        for b in b_range.0..b_range.1 {
+        for b in range.0..range.1 {
             if is_euler_triangle(a, b) {
                 if !is_duplicate(a, b, &triangles) {
                     triangles.push((a, b));
@@ -34,16 +49,18 @@ fn find_euler_triangles(a_range: (u64, u64), b_range: (u64, u64)) -> Vec<(u64, u
         }
     }
 
-    return triangles;
+    triangles
 }
 
 fn main() {
-    let range = (1, 10_000);
+    let range = (1, 1_000_000);
+    let triangles = find_euler_triangles(range);
 
-    let triangles = find_euler_triangles(range, range);
-
-    println!("Found {} euler triangles:", triangles.len());
-    for t in triangles.iter() {
-        println!("  {}, {}, {}", t.0, t.1, (t.0 * t.0) + (t.1 * t.1).isqrt());
-    }
+    store_triangles(&triangles, "triangles.txt");
+    println!(
+        "Found {} euler triangles in range {}, {}",
+        triangles.len(),
+        range.0,
+        range.1
+    );
 }
