bf45224a615cafd6f9b3e269eba2f2ac1ecba497
J <133714506+J-Feltens@users.noreply.github.com>
2026-03-14 15:36:19 +0100

    test on cstbeast

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 #![allow(warnings)]
 
+use std::env;
 mod util;
 use crate::util::run_ultimate_speed;
 fn main() {
-    run_ultimate_speed(1000, 1000);
+    let args: Vec<String> = env::args().collect();
+    let mut max_side_length = 1000 as u64;
+
+    if args.len() >= 2 {
+        max_side_length = args[1]
+            .parse::<u64>()
+            .expect("Error: Max side length count must be a positive integer");
+    } else {
+        println!("No max side length specified, defaulting to 1000");
+    }
+
+    run_ultimate_speed(max_side_length, max_side_length);
 }
